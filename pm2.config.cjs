44276907369fcf35ab43670ca5d868290a1a4a84
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-12-19 17:48:16 +0100

    pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,9 +3,11 @@ module.exports = {
     {
       name: "monitor",
       script: "yarn monitor",
-      max_restarts: 10,
       restart_delay: 60 * 1000 * 15,
       log_date_format: "YYYY-MM-DD HH:mm Z",
+      env: {
+        NODE_ENV: "production",
+      },
     },
   ],
 };
